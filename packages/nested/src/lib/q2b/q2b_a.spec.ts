498b7104156e7f3f11d4abaee616cf618b15b6e9
quantapix <quantapix@gmail.com>
2022-07-27 23:45:32 -0400

    qpx

---
+++ packages/nested/src/lib/q2b/q2b_a.spec.ts
@@ -0,0 +1,35 @@
+import * as q1 from "../q1"
+import * as q2 from "../q2"
+import { newFrame } from "./q2b_a"
+beforeAll(() => {})
+describe("modd", () => {
+  const qf = newFrame(q2.newFrame(q1.newFrame({})))
+  const a = new q1.A().update(1)
+  const b = new q1.B().update(a)
+  const c = new q1.C().update([b])
+  beforeEach(() => {})
+  test("xxx", () => {
+    expect(qf.x.xx.xxx.f(c)).toBeTruthy
+    expect(qf.x.xx.xxx.f(b)).toBeTruthy
+    expect(qf.x.xx.xxx.f(a)).toBeTruthy
+    expect(qf.x.xx.xxx.g(c)).toBeFalsy
+    expect(qf.x.xx.xxx.g(b)).toBeFalsy
+    expect(qf.x.xx.xxx.g(a)).toBeFalsy
+  })
+  test("xx", () => {
+    expect(qf.x.xx.f(c)).toBeFalsy
+    expect(qf.x.xx.f(b)).toBeTruthy
+    expect(qf.x.xx.f(a)).toBeTruthy
+    expect(qf.x.xx.g(c)).toBeTruthy
+    expect(qf.x.xx.g(b)).toBeFalsy
+    expect(qf.x.xx.g(a)).toBeFalsy
+  })
+  test("x", () => {
+    expect(qf.x.f(c)).toBeFalsy
+    expect(qf.x.f(b)).toBeFalsy
+    expect(qf.x.f(a)).toBeTruthy
+    expect(qf.x.f(c)).toBeTruthy
+    expect(qf.x.f(b)).toBeTruthy
+    expect(qf.x.f(a)).toBeFalsy
+  })
+})
---
+++ packages/nested/src/lib/q3/q3_a.spec.ts
@@ -0,0 +1,24 @@
+import * as qt from "../q0_b"
+import * as q1 from "../q1"
+import * as q3 from "./q3_a"
+beforeAll(() => {})
+describe("mode", () => {
+  const a = new q1.A().update(1)
+  const b = new q1.B().update(a)
+  const c = new q1.C().update([b])
+  const ab = new q3.AB().update(111)
+  const bc = new q3.BC().update(222)
+  const ns1 = [a, b, ab, c, bc]
+  beforeEach(() => {})
+  test("walk", () => {
+    expect(ns1.filter(n => n.walk(n => !!n?.n2)).length).toBe(2)
+  })
+  test("walks", () => {
+    const ns2 = new q1.Nodes(...ns1)
+    function walks(ns?: qt.Nodes) {
+      const vs = ns?.map(n => n.n2)
+      return vs?.filter(n => !!n).length
+    }
+    expect(ns2.walk(undefined, walks)).toBe(2)
+  })
+})
