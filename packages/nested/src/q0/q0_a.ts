5ba8b7b2e23f234ce665240f77918904b4d8c57f
quantapix <quantapix@gmail.com>
2022-08-17 13:27:26 -0400

    qpx

--- packages/nested/src/q0/q0_a.ts
+++ packages/nested/src/q0/q0_a.ts
@@ -1,33 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export class Data {
-  readonly d1 = 123;
-  d2?: number;
+  readonly d1 = 123
+  d2?: number
 }
 export interface Frame {
-  get: Fget;
-  is: Fis;
+  get: Fget
+  is: Fis
 }
 export class Fget {
   empty() {
-    return {};
+    return {}
   }
 }
 export class Fis {
   string(x: unknown): x is string {
-    return typeof x === 'string';
+    return typeof x === "string"
   }
 }
 export function newFrame() {
-  return { get: new Fget(), is: new Fis() } as Frame;
+  return { get: new Fget(), is: new Fis() } as Frame
 }
-export const qf: Frame = newFrame();
+export const qf: Frame = newFrame()
 
 export function addMixins(t: any, ss: any[]) {
   ss.forEach((s: any) => {
-    const p = s.prototype || s.__proto__;
-    Object.getOwnPropertyNames(p).forEach((n) => {
-      if (n == 'constructor') return;
-      //console.log(`adding ${s.name}.${n}`);
-      Object.defineProperty(t.prototype || t.__proto__, n, Object.getOwnPropertyDescriptor(p, n)!);
-    });
-  });
+    const p = s.prototype || s.__proto__
+    Object.getOwnPropertyNames(p).forEach(n => {
+      if (n == "constructor") return
+      const v = Object.getOwnPropertyDescriptor(p, n)
+      if (v != undefined) {
+        //console.log(`adding ${s.name}.${n}`);
+        Object.defineProperty(t.prototype || t.__proto__, n, v)
+      }
+    })
+  })
 }
