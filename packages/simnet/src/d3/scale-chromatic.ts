1b4c50ef6f752c34bccfe8b9da53af7291fcab84
quantapix <quantapix@gmail.com>
2022-10-26 10:16:38 -0400

    qpx

--- packages/simnet/src/d3/scale-chromatic.ts
+++ packages/simnet/src/d3/scale-chromatic.ts
@@ -1,5 +1,5 @@
 import { cubehelix, rgb } from "./color.js"
-import { interpolateCubehelixLong, interpolateRgbBasisClosed, interpolateRgbBasis } from "./interpolate.js"
+import { cubehelixLong, rgbBasisClosed, rgbBasis } from "./interpolate.js"
 import { scaleSequential } from "./scale.js"
 
 export function colors(x: string) {
@@ -10,19 +10,27 @@ export function colors(x: string) {
   return y
 }
 
-export const schemeAccent = colors("7fc97fbeaed4fdc086ffff99386cb0f0027fbf5b17666666")
-export const schemeDark2 = colors("1b9e77d95f027570b3e7298a66a61ee6ab02a6761d666666")
-export const schemePaired = colors("a6cee31f78b4b2df8a33a02cfb9a99e31a1cfdbf6fff7f00cab2d66a3d9affff99b15928")
-export const schemePastel1 = colors("fbb4aeb3cde3ccebc5decbe4fed9a6ffffcce5d8bdfddaecf2f2f2")
-export const schemePastel2 = colors("b3e2cdfdcdaccbd5e8f4cae4e6f5c9fff2aef1e2cccccccc")
-export const schemeSet1 = colors("e41a1c377eb84daf4a984ea3ff7f00ffff33a65628f781bf999999")
-export const schemeSet2 = colors("66c2a5fc8d628da0cbe78ac3a6d854ffd92fe5c494b3b3b3")
-export const schemeSet3 = colors("8dd3c7ffffb3bebadafb807280b1d3fdb462b3de69fccde5d9d9d9bc80bdccebc5ffed6f")
-export const schemeTableau10 = colors("4e79a7f28e2ce1575976b7b259a14fedc949af7aa1ff9da79c755fbab0ab")
-export const schemeCategory10 = colors("1f77b4ff7f0e2ca02cd627289467bd8c564be377c27f7f7fbcbd2217becf")
+export const schemeAccent: ReadonlyArray<string> = colors("7fc97fbeaed4fdc086ffff99386cb0f0027fbf5b17666666")
+export const schemeCategory10: ReadonlyArray<string> = colors(
+  "1f77b4ff7f0e2ca02cd627289467bd8c564be377c27f7f7fbcbd2217becf"
+)
+export const schemeDark2: ReadonlyArray<string> = colors("1b9e77d95f027570b3e7298a66a61ee6ab02a6761d666666")
+export const schemePaired: ReadonlyArray<string> = colors(
+  "a6cee31f78b4b2df8a33a02cfb9a99e31a1cfdbf6fff7f00cab2d66a3d9affff99b15928"
+)
+export const schemePastel1: ReadonlyArray<string> = colors("fbb4aeb3cde3ccebc5decbe4fed9a6ffffcce5d8bdfddaecf2f2f2")
+export const schemePastel2: ReadonlyArray<string> = colors("b3e2cdfdcdaccbd5e8f4cae4e6f5c9fff2aef1e2cccccccc")
+export const schemeSet1: ReadonlyArray<string> = colors("e41a1c377eb84daf4a984ea3ff7f00ffff33a65628f781bf999999")
+export const schemeSet2: ReadonlyArray<string> = colors("66c2a5fc8d628da0cbe78ac3a6d854ffd92fe5c494b3b3b3")
+export const schemeSet3: ReadonlyArray<string> = colors(
+  "8dd3c7ffffb3bebadafb807280b1d3fdb462b3de69fccde5d9d9d9bc80bdccebc5ffed6f"
+)
+export const schemeTableau10: ReadonlyArray<string> = colors(
+  "4e79a7f28e2ce1575976b7b259a14fedc949af7aa1ff9da79c755fbab0ab"
+)
 
 export function ramp(x: any) {
-  return interpolateRgbBasis(x[x.length - 1])
+  return rgbBasis(x[x.length - 1])
 }
 
 export const schemeBrBG = new Array(3)
@@ -416,10 +424,10 @@ export function interpolateCividis(x: number) {
   )
 }
 
-export const interpolateCubehelixDefault = interpolateCubehelixLong(cubehelix(300, 0.5, 0.0), cubehelix(-240, 0.5, 1.0))
+export const interpolateCubehelixDefault = cubehelixLong(cubehelix(300, 0.5, 0.0), cubehelix(-240, 0.5, 1.0))
 
-export const interpolateWarm = interpolateCubehelixLong(cubehelix(-100, 0.75, 0.35), cubehelix(80, 1.5, 0.8))
-export const interpolateCool = interpolateCubehelixLong(cubehelix(260, 0.75, 0.35), cubehelix(80, 1.5, 0.8))
+export const interpolateWarm = cubehelixLong(cubehelix(-100, 0.75, 0.35), cubehelix(80, 1.5, 0.8))
+export const interpolateCool = cubehelixLong(cubehelix(260, 0.75, 0.35), cubehelix(80, 1.5, 0.8))
 
 const c = cubehelix()
 
@@ -496,7 +504,7 @@ export const interpolatePlasma = ramp2(
 )
 
 export function rampClosed(range: any) {
-  const s = scaleSequential(interpolateRgbBasisClosed(colors(range))).clamp(true)
+  const s = scaleSequential(rgbBasisClosed(colors(range))).clamp(true)
   delete s.clamp
   return s
 }
